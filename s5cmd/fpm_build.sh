5b6e4a35e6d0e18ee25996b8f96610fc29c7f44e
Elia Pinto <pinto.elia@gmail.com>
2022-04-28 08:49:12 +0200

    add s5cmd

---
+++ s5cmd/fpm_build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+NAME=s5cmd
+VERSION=1.4.0
+RELEASE=1
+ARCH="x86_64"
+DISTTAG="el8"
+PACKAGENAME="${NAME}-${VERSION}-${RELEASE}.$ARCH.$DISTTAG.rpm"
+INPUT=${NAME}_${VERSION}_Linux-64bit.tar.gz
+URL="https://github.com/peak/s5cmd/releases/download/v${VERSION}/"
+SUMMARY="s5cmd is a very fast S3 and local filesystem execution tool."
+PREFIX=/opt/sddc
+BINDIR=${PREFIX}/bin
+SBINDIR=${PREFIX}/sbin
+MAINTAINER="Private FPM project"
+LICENSE="MIT"
+VENDOR="Private FPM Project"
+AUTHOR="Elia Pinto <pinto.elia@gmail.com>"
+DEFATTRFILE=755
+##########
+trap 'rm -rf ${TMPDIR} ${TMPFILE_CHANGELOG}' EXIT
+tempdir() {
+    tempprefix=${0##*/}
+    mktemp -d /tmp/${tempprefix}.XXXXXX
+}
+tempfile_changelog() {
+    local tempprefix="${0##*/}"
+    mktemp  /tmp/"${tempprefix}".XXXXXX
+}
+TMPDIR="$(tempdir)"
+TMPFILE_CHANGELOG="$(tempfile_changelog)"
+cat <<EOF>"$TMPFILE_CHANGELOG"
+* $(LANG=C date "+%a %b %d %Y") ${AUTHOR} - ${VERSION}-${RELEASE}
+- First Release. 
+EOF
+tar -C $TMPDIR -zxf $INPUT
+rm -f "${PACKAGENAME}"
+fpm -s dir -t rpm  -p ${PACKAGENAME} -n ${NAME} -v ${VERSION} --iteration ${RELEASE} -a ${ARCH} -m "${MAINTAINER}" --rpm-summary "${SUMMARY}" --description "${SUMMARY}" --rpm-defattrfile "$DEFATTRFILE" --url "${URL}" --vendor "$VENDOR" --license "$LICENSE" -C $TMPDIR s5cmd=${BINDIR}/${NAME}
